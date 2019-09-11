00684278106b89c728f65c7793ca79955c0702bd
januwA <ajanuw1641845087@sina.com>
2019-09-11 14:46:57 +0800

    gist

--- js/content.js
+++ js/content.js
@@ -1,14 +1,29 @@
 // 标签页中的内容脚本
 
-const pres = document.querySelectorAll("pre");
-
+// pre
+const pres = document.querySelectorAll('pre');
 for (let i = 0; i < pres.length; i++) {
   const pre = pres[i];
   const cls = pre.childNodes;
   if (!cls || cls.length === 0) continue;
 
   const firstChild = cls[0];
-  if (firstChild.nodeName !== "CODE") {
+  if (firstChild.nodeName !== 'CODE') {
     pre.innerHTML = `<code>${pre.innerHTML}</code>`;
   }
 }
+
+function checkGist() {
+  // gist
+  const gists = document.querySelectorAll('.gist');
+  // console.log(`gists length: ${gists.length}`);
+
+  for (let i = 0; i < gists.length; i++) {
+    const gist = gists[i];
+    const codeElement = document.createElement('code');
+    codeElement.innerHTML = gist.outerHTML;
+    gist.replaceWith(codeElement);
+  }
+}
+
+checkGist();
