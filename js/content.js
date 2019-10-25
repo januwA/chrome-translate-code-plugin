7644e9b973dd85abede943f123e4f70353d472be
januwA <ajanuw1641845087@sina.com>
2019-10-25 21:16:46 +0800

    定时检查，避免异步数据

--- js/content.js
+++ js/content.js
@@ -1,23 +1,25 @@
 // 标签页中的内容脚本
 
 // pre
-const pres = document.querySelectorAll('pre');
-for (let i = 0; i < pres.length; i++) {
-  const pre = pres[i];
-  const cls = pre.childNodes;
-  if (!cls || cls.length === 0) continue;
+function checkPres() {
+  const pres = document.querySelectorAll('pre');
+  for (let i = 0; i < pres.length; i++) {
+    const pre = pres[i];
+    const cls = pre.childNodes;
+    if (!cls || cls.length === 0) continue;
 
-  const firstChild = cls[0];
-  if (firstChild.nodeName !== 'CODE') {
-    pre.innerHTML = `<code>${pre.innerHTML}</code>`;
+    const firstChild = cls[0];
+    if (firstChild.nodeName !== 'CODE') {
+      pre.innerHTML = `<code>${pre.innerHTML}</code>`;
+    }
   }
 }
+
+checkPres();
 
 function checkGist() {
   // gist
   const gists = document.querySelectorAll('.gist');
-  // console.log(`gists length: ${gists.length}`);
-
   for (let i = 0; i < gists.length; i++) {
     const gist = gists[i];
     const codeElement = document.createElement('code');
@@ -27,3 +29,8 @@ function checkGist() {
 }
 
 checkGist();
+
+setInterval(() => {
+  checkPres();
+  checkGist();
+}, 1200);
