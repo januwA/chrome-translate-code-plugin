22bee57aabcce4298bba47b3036b43cb856afce8
januwA <ajanuw1641845087@sina.com>
2020-01-11 19:24:27 +0800

    修复

--- js/content.js
+++ js/content.js
@@ -2,18 +2,24 @@
 
 // pre
 function checkPres() {
-  const pres = document.querySelectorAll('pre');
+  const pres = document.querySelectorAll("pre");
   for (let i = 0; i < pres.length; i++) {
     const pre = pres[i];
-    if(pre.innerHTML.trim()){
+    if (pre.innerHTML.trim()) {
       const cls = pre.children;
       if (cls.length === 0) {
         pre.innerHTML = `<code>${pre.innerHTML}</code>`;
-      }else{
-        const firstChild = cls[0];
-        if (firstChild.nodeName !== 'CODE') {
-          pre.innerHTML = `<code>${pre.innerHTML}</code>`;
+      } else {
+        if (
+          pre.children.length === 1 &&
+          pre.firstElementChild.nodeName == "CODE"
+        ) {
+          continue;
         }
+        const nmodes = Array.from(pre.childNodes);
+        const code = document.createElement("code");
+        nmodes.forEach(e => code.appendChild(e));
+        pre.appendChild(code);
       }
     }
   }
@@ -23,11 +29,11 @@ checkPres();
 
 function checkGist() {
   // gist
-  const gists = document.querySelectorAll('.gist');
+  const gists = document.querySelectorAll(".gist");
   for (let i = 0; i < gists.length; i++) {
     const gist = gists[i];
-    if(gist.parentElement && gist.parentElement.nodeName === 'CODE') continue;
-    const codeElement = document.createElement('code');
+    if (gist.parentElement && gist.parentElement.nodeName === "CODE") continue;
+    const codeElement = document.createElement("code");
     codeElement.innerHTML = gist.outerHTML;
     gist.replaceWith(codeElement);
   }
--- js/medium-content.js
+++ js/medium-content.js
@@ -2,11 +2,33 @@
 
 setInterval(() => {
   const figures = document.querySelectorAll("figure");
-  for (const figure in Array.from(figures)) {
+  for (const figure of Array.from(figures)) {
     if (figure && figure.parentElement.nodeName !== "CODE") {
       const code = document.createElement("code");
       figure.before(code);
       code.appendChild(figure);
+    }
+  }
+}, 2000);
+
+setInterval(() => {
+  const pres = document.querySelectorAll("pre");
+  for (const pre of Array.from(pres)) {
+    if (pre && pre.innerHTML && pre.innerHTML.trim()) {
+      if (pre.children.length === 0) {
+        pre.innerHTML = `<code>${pre.innerHTML}</code>`;
+      } else {
+        if (
+          pre.children.length === 1 &&
+          pre.firstElementChild.nodeName == "CODE"
+        ) {
+          continue;
+        }
+        const nmodes = Array.from(pre.childNodes);
+        const code = document.createElement("code");
+        nmodes.forEach(e => code.appendChild(e));
+        pre.appendChild(code);
+      }
     }
   }
 }, 2000);
